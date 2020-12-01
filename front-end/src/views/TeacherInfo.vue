3e9e56e720115e288853a9ef96ab928b39f98598
wademcmillan <wade.mcmillan@outlook.com>
2020-11-30 17:27:22 -0700

    thingie

--- front-end/src/views/TeacherInfo.vue
+++ front-end/src/views/TeacherInfo.vue
@@ -34,20 +34,18 @@
 		</tr>
 	  </table>
 	</div>
-	
 	<h3>Teach a New Class</h3>
 	<form>
 	  <label for="class_id">class id:  </label>
-	  <input type="text" id="class_id" name="class_id"><br><br>
+	  <input type="text" id="class_id" v-model ="class_id" name="class_id"><br><br>
 	  <label for="class_name">Class Name:</label>
-	  <input type="text" id="class_name" name="class_name"><br><br>
+	  <input type="text" id="class_name" v-model ="class_name" name="class_name"><br><br>
 	  <label for="class_hour">class hour:  </label>
-	  <input type="text" id="class_hour" name="class_hour"><br><br>
+	  <input type="text" id="class_hour" v-model ="class_hour" name="class_hour"><br><br>
 	  <label for="class_day">Class Day:</label>
-	  <input type="text" id="class_day" name="class_day"><br><br>
-	  
-	  <input type="submit" value="Submit" @click="doSomething">
+	  <input type="text" id="class_day" v-model ="class_day" name="class_day"><br><br>
 	</form>
+	<button @click="addClass()">Add Class</button>
 
 </div>
 
@@ -56,7 +54,16 @@
 </template>
 
 <script>
+import axios from 'axios';
 export default {
+	data() {
+	  return {
+			class_id: "",
+			class_name: "",
+			class_hour: "",
+			class_day: "",
+		}
+	},
   created() {
     this.$getClasses();
 	this.$getStudents();
@@ -70,14 +77,17 @@ export default {
     },
 	classes() {
 	  let myClasses = this.$root.$data.classes.filter(classe => classe.prof_id == Number(this.$route.params.id));
-    console.log(myClasses)
 	  return myClasses;
 	}
   },
   methods: {
-	doSomething() {
-		alert("Something");
-	}
+		async addClass() {
+			alert("banger");
+			let thingie = {class_id: this.class_id, class_name: this.class_name, prof_id: this.$route.params.id, class_hour: this.class_hour, class_day: this.class_day};
+		  await axios.post('/api/classes', thingie)
+
+			window.location.reload(false);
+		}
   }
 }
 </script>
